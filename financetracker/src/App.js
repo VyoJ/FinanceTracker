de103a69a90e5561dbe2636fac2a87bf24e81206
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2023-10-18 09:39:42 +0530

    First commit

---
+++ financetracker/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-center text-blue-500">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
