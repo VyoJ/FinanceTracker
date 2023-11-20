8a2c5c2045e650f3fdffcef9ffef1106979e74e5
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2023-11-20 11:33:08 +0530

    Background fix

--- financetracker/src/Components/Layout.jsx
+++ financetracker/src/Components/Layout.jsx
@@ -14,7 +14,10 @@ const Layout = ({ children }) => {
     return (
       <>
         <Navbar />
-        <div className="h-[calc(100vh-176px)] bg-no-repeat bg-[url('https://wallpaperboat.com/wp-content/uploads/2021/04/15/75170/finance-04.jpg')] bg-gray-700">
+        <div
+          style={{ background: `url("/bg3.jpg")` }}
+          className={`h-[calc(100vh-176px)] bg-no-repeat`}
+        >
           {children}
         </div>
         <Footer />
--- financetracker/src/Routes/Home.jsx
+++ financetracker/src/Routes/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <section className="bg-center bg-no-repeat bg-[url('https://wallpaperboat.com/wp-content/uploads/2021/04/15/75170/finance-04.jpg')] bg-gray-700 bg-blend-multiply">
+    <section className="">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
           Reach your hidden financial potential
